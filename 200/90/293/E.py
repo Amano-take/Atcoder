7deb5ac6ce9e5f056923bc8b4744008abccb5755
amano <amano>
2024-01-02 08:34:24 +0900

    commit: commit

--- 200/90/293/E.py
+++ 200/90/293/E.py
@@ -5,7 +5,24 @@ from itertools import product
 inf = float("inf")
 sys.setrecursionlimit(10**8)
 _INPUT = """\
-
+1000000000 1000000000000 998244353
 """
 sys.stdin = io.StringIO(_INPUT)
 input=lambda: sys.stdin.readline().strip()
+A, X, M = map(int, input().split())
+#ans is A^0 + A^1 + A^2 + ... + A^X-1 mod M
+#(A^0 + A^1 + ... + A^(X-1)) * (A - 1) = A^X - 1
+#(A^0 + A^1 + ... + A^(X-1)) = (A^X - 1) / (A - 1)
+
+if A == 1:
+    print(X % M)
+    exit()
+
+
+ans = pow(A, X, M) - 1
+div = pow(A-1, -1, M)
+print(ans * div % M)
+
+for i in range(1, 16):
+    div = pow(i, -1, 16)
+    print(div, div*i % 16)
