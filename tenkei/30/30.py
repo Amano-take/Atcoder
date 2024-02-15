910c091787636b42edf09aeb752c20a153b772c4
Amano-take <63344408+Amano-take@users.noreply.github.com>
2024-02-16 04:29:17 +0900

    2024/02/16 04:30:30

--- tenkei/30/30.py
+++ tenkei/30/30.py
@@ -5,7 +5,22 @@ from itertools import product
 inf = float("inf")
 sys.setrecursionlimit(10**8)
 _INPUT = """\
-
+10000000 3
 """
 sys.stdin = io.StringIO(_INPUT)
 input=lambda: sys.stdin.readline().strip()
+
+N, K = map(int, input().split())
+
+# eratosthenes'
+
+thieve = [0] * (N + 1)
+thieve[0] = 0
+ans = 0
+for i in range(2, N+1):
+    if thieve[i] == 0:
+        for j in range(i, N+1, i):
+            thieve[j] += 1
+    if thieve[i] >= K:
+        ans += 1
+print(ans)
--- tenkei/30/31.py
+++ tenkei/30/31.py
@@ -5,7 +5,48 @@ from itertools import product
 inf = float("inf")
 sys.setrecursionlimit(10**8)
 _INPUT = """\
-
+6
+3 1 4 1 5 9
+2 7 1 8 2 8
 """
 sys.stdin = io.StringIO(_INPUT)
 input=lambda: sys.stdin.readline().strip()
+
+def mex(s):
+    for i in range(2000):
+        if i not in s:
+            return i
+    return 2000
+
+N = int(input())
+Ws = list(map(int, input().split()))
+Bs = list(map(int, input().split()))
+grandy = ddict(int)
+for i in range(51):
+    for j in range(1326):
+        if i == 0 and (j == 0 or j == 1):
+            grandy[(i, j)] = 0
+            continue
+        
+        s = set()
+        if i > 0:
+            s.add(grandy[(i-1, j+i)])
+        if j > 1:
+            k = j // 2
+            for l in range(1, k+1):
+                s.add(grandy[(i, j-l)])
+        grandy[(i, j)] = mex(s)
+
+
+
+g = []
+for w, b in zip(Ws, Bs):
+    g.append(grandy[(w, b)])
+
+xorsum = 0
+for i in g:
+    xorsum ^= i
+if xorsum == 0:
+    print("Second")
+else:
+    print("First")
