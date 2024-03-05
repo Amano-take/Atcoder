a2c784eb9029c433da1cd2487a9aab564836a92c
amano <takehiro0927@icloud.com>
2024-03-06 07:32:30 +0900

    commit

--- tenkei/20/27.py
+++ tenkei/20/27.py
@@ -5,7 +5,20 @@ from itertools import product
 inf = float("inf")
 sys.setrecursionlimit(10**8)
 _INPUT = """\
-
+5
+e869120
+atcoder
+e869120
+square1001
+square1001
 """
 sys.stdin = io.StringIO(_INPUT)
 input=lambda: sys.stdin.readline().strip()
+
+N = int(input())
+name = set()
+for i in range(N):
+    temp = input()
+    if temp not in name:
+        name.add(temp)
+        print(i+1)
