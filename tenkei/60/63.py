5485590498d0578a6427fe30dd770d4f72f1c237
amano-take <reol0927@i.softbank.jp>
2024-03-23 16:17:40 +0900

    2024-03-23 16:17:34

--- tenkei/60/63.py
+++ tenkei/60/63.py
@@ -1,11 +1,33 @@
 import sys, io
 import math, heapq, bisect
 from collections import deque, defaultdict as ddict
-from itertools import product
+from itertools import product, combinations
 inf = float("inf")
 sys.setrecursionlimit(10**8)
 _INPUT = """\
-
+4 6
+1 1 1 1 1 2
+1 2 2 2 2 2
+1 2 2 3 2 3
+1 2 3 2 2 3
 """
 sys.stdin = io.StringIO(_INPUT)
 input=lambda: sys.stdin.readline().strip()
+H, W = map(int, input().split())
+grid = [list(map(int, input().split())) for _ in range(H)]
+ans = 0
+for bits in range(1, 1 << H):
+    rows = []
+    for i in range(H):
+        if bits >> i & 1:
+            rows.append(i)
+    dic = ddict(int)
+    for w in range(W):
+        s = set()
+        for r in rows:
+            s.add(grid[r][w])
+        if len(s) == 1:
+            dic[list(s)[0]] += 1
+    ans = max(ans, max(dic.values()) * len(rows))
+print(ans)
+
--- tenkei/60/64.py
+++ tenkei/60/64.py
@@ -5,7 +5,32 @@ from itertools import product
 inf = float("inf")
 sys.setrecursionlimit(10**8)
 _INPUT = """\
-
+3 3
+1 2 3
+2 3 1
+1 2 -1
+1 3 2
 """
 sys.stdin = io.StringIO(_INPUT)
 input=lambda: sys.stdin.readline().strip()
+N, Q = map(int, input().split())
+A = list(map(int, input().split()))
+E = []
+for i in range(1, N):
+    E.append(A[i-1] - A[i])
+
+ans = sum(abs(e) for e in E)
+
+for q in range(Q):
+    l, r, v = map(int, input().split())
+    l -= 2
+    r -= 1
+    if l >= 0:
+        ans -= abs(E[l])
+        E[l] -= v
+        ans += abs(E[l])
+    if r <= N - 2:
+        ans -= abs(E[r])
+        E[r] += v
+        ans += abs(E[r])
+    print(ans)
